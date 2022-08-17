8475969550cbebf18132b6c214ce3745ccf8ad7f
btrinh426 <btrinh426@yahoo.com>
2022-08-17 15:48:52 -0700

    finished form login

--- src/loginPage/Login.js
+++ src/loginPage/Login.js
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import loginPage from "../scss/loginPage.scss";
 import pokeLogo from "../images/pokeLogo.png";
 
 function LoginPage() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUserNameChange = (event) => {
+    setUserName(event.target.value);
+    // console.log(userName, "username change");
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    // console.log(password, "password change");
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(userName, "userName submitted");
+    console.log(password, "password submitted");
+  };
+
   return (
     <div className="wrapper">
       <div className="mainContainer">
         <img className="pokeLogo" src={pokeLogo} alt="pokeLogo" />
         <div className="loginWrapper">
-          <div className="login">
-             User Name:
-            <input type="text" placeholder="username" className="login"></input>
+          <div className="loginContainer">
+            <form onSubmit={handleSubmit}>
+              <label className="login">
+                User Name:
+                <input
+                  type="text"
+                  className="login"
+                  placeholder="username"
+                  value={userName}
+                  onChange={handleUserNameChange}
+                />
+              </label>
+              <label className="login">
+                Password:
+                <input
+                  type="text"
+                  className="login"
+                  placeholder="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+              </label>
+              <button>Login</button>
+            </form>
           </div>
-          <div className="login">
-             Password:
-            <input type="text" placeholder="password" className="login"></input>
-          </div>
-          <button>Login</button>
-
-         </div>
+        </div>
       </div>
     </div>
   );
