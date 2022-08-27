0e030e57d06e3b1debfbbad0a4895d6aff948e15
btrinh426 <btrinh426@yahoo.com>
2022-08-27 13:20:32 -0700

    almost done with login auth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
+import useToken from "./pages/Login/UseToken";
 import Tastea from "./pages/Tastea/Tastea";
 import OmniHorse from "./pages/OmniHorse/OmniHorse";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const setToken = async (userToken) => {
-  sessionStorage.setItem("token", JSON.stringify(userToken));
-};
+// const setToken = async (userToken) => {
+//   sessionStorage.setItem("token", JSON.stringify(userToken));
+// };
 
-const getToken = () => {
-  const tokenString = sessionStorage.getItem("token");
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token;
-};
+// const getToken = () => {
+//   const tokenString = sessionStorage.getItem("token");
+//   const userToken = JSON.parse(tokenString);
+//   return userToken?.token;
+// };
 
 function App() {
-  const token = getToken();
+  const { token, setToken } = useToken();
+
+  // if (!token) {
+  //   return <Login setToken={setToken} />;
+  // }
 
   return (
     <BrowserRouter>
---
+++ src/pages/Login/UseToken.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const UseToken = () => {
+  const getToken = () => {
+    const tokenString = sessionStorage.getItem("token");
+    const userToken = JSON.parse(tokenString);
+    return userToken?.token;
+  };
+
+  const [token, setToken] = useState();
+
+  const saveToken = (userToken) => {
+    sessionStorage.setItem("token", JSON.stringify(userToken));
+    setToken(userToken.token);
+  };
+
+  return {
+    setToken: saveToken,
+    token,
+  };
+};
+
+export default UseToken;
